47070d8019ad49a31152b047da613974975218c5
Patrick Seidel <patrick.seidel@stud.hn.de>
2016-04-27 14:19:00 +0200

    Kommentierte Version

--- OOP1_Uebungen/src/Insertionsort.java
+++ OOP1_Uebungen/src/Insertionsort.java
@@ -1,18 +1,18 @@
 
 public class Insertionsort {
 	public static void main (String [] args){
-		int [] liste = new int [10];
+		int [] liste = new int [10]; 			// Deklaration Variablen
 		int puffer = 0;
 		int i,j,k;
 		
 		for (i = 0; i < liste.length; i++){
-			liste[i] = (int) (Math.random() * 100);
+			liste[i] = (int) (Math.random() * 100); // Initialisierung Array
 			System.out.print(liste[i] + ", ");
 		}
-		for (j = 1; j < liste.length; j++){
-			puffer = liste[j];
+		for (j = 1; j < liste.length; j++){	
+			puffer = liste[j];			// Übergabe des zu sortierenden Wertes in den puffer
 			for(k = j; k > 0 && liste[k-1] > puffer; k--){
-				liste[k] = liste[k-1]; 
+				liste[k] = liste[k-1]; 		
 			}
 			liste[k] = puffer;				
 		}
